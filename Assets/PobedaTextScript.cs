7edd49036e6e54f67893ea259f730ea4ed967089
alljay3 <mihaspiva@yandex.ru>
2023-06-09 02:54:47 +0800

    FinishedDemoVersion

--- Assets/PobedaTextScript.cs
+++ Assets/PobedaTextScript.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
 public class PobedaTextScript : MonoBehaviour
 {
+
+    [SerializeField] GameObject Music;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Music.GetComponent<AudioSource>().enabled = false;
     }
     public void Pobeda()
     {
